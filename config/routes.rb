8cc5ffc3ed5e94a74aa1774fbb88299e89254dad
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-04-10 10:25:39 +0100

    authentication added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
 
+  autenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
+
   resources :docs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
